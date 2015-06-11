60138826325a90a257c6e6195a2eeaee38cdb132
DC* <dario.cavuotti@gmail.com>
2015-06-11 05:52:09 -0300

    [WIP] Moved -antigen-dump-file-list function to zcache extension

--- ext/zcache.zsh
+++ ext/zcache.zsh
@@ -6,6 +6,66 @@ export _ANTIGEN_BUNDLE_CACHE_LOAD=$_ANTIGEN_CACHE_DIR/.zcache.load
 [[ ! -e $_ANTIGEN_CACHE_DIR ]] && mkdir $_ANTIGEN_CACHE_DIR
 local extensions_paths
 
+# TODO Merge this code with -antigen-load function to avoid duplication
+-antigen-dump-file-list () {
+
+    local url="$1"
+    local loc="$2"
+    local make_local_clone="$3"
+
+    # The full location where the plugin is located.
+    local location
+    if $make_local_clone; then
+        location="$(-antigen-get-clone-dir "$url")/"
+    else
+        location="$url/"
+    fi
+
+    [[ $loc != "/" ]] && location="$location$loc"
+
+    if [[ -f "$location" ]]; then
+        echo "$location"
+
+    else
+
+        # Source the plugin script.
+        # FIXME: I don't know. Looks very very ugly. Needs a better
+        # implementation once tests are ready.
+        local script_loc="$(ls "$location" | grep '\.plugin\.zsh$' | head -n1)"
+
+        if [[ -f $location/$script_loc ]]; then
+            # If we have a `*.plugin.zsh`, source it.
+            echo "$location/$script_loc"
+
+        elif [[ -f $location/init.zsh ]]; then
+            # If we have a `init.zsh`
+            if (( $+functions[pmodload] )); then
+                # If pmodload is defined pmodload the module. Remove `modules/`
+                # from loc to find module name.
+                #pmodload "${loc#modules/}"
+            else
+                # Otherwise source it.
+                echo "$location/init.zsh"
+            fi
+
+        elif ls "$location" | grep -l '\.zsh$' &> /dev/null; then
+            # If there is no `*.plugin.zsh` file, source *all* the `*.zsh`
+            # files.
+            for script ($location/*.zsh(N)) { echo "$script" }
+
+        elif ls "$location" | grep -l '\.sh$' &> /dev/null; then
+            # If there are no `*.zsh` files either, we look for and source any
+            # `*.sh` files instead.
+            for script ($location/*.sh(N)) { echo "$script" }
+
+        fi
+
+        # Add to $fpath, for completion(s).
+        #fpath=($location $fpath)
+
+    fi
+}
+
 function -dots-start-capture () {
     dots__capture__file=$1
     dots__capture__file_load=$2
